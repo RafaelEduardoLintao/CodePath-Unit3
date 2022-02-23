89daed4315311e4fe3c70f9648c14953a9d16419
Rafael Eduardo Lintao <relintao@gmail.com>
2022-02-23 14:07:17 -0800

    View profile pic, username and tweets

--- twitter_ios_starter/Twitter/HomeTableTableViewController.swift
+++ twitter_ios_starter/Twitter/HomeTableTableViewController.swift
@@ -10,29 +10,77 @@ import UIKit
 
 class HomeTableTableViewController: UITableViewController {
 
+    var tweetArray = [NSDictionary]()
+    var numberOfTweets: Int!
+    
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
-    }
+    }//end onLogout
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        let user = tweetArray[indexPath.row]["user"] as! NSDictionary
+        
+        
+        cell.usernameLabel.text = user["name"] as? String
+        cell.tweetsContent.text = (tweetArray[indexPath.row]["text"] as! String)
+        
+        
+        let imageURL = URL(string: (user["profile_image_url_https"] as? String)!)
+        let data = try? Data(contentsOf: imageURL!)
+        
+        if let imageData = data{
+            cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
+        return cell
+    }//end tableView func
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadTweet()
+    
+    }//end viewDidLoad
+    
+    
+    //Function that uses the API to load tweets
+    func loadTweet(){
+        
+        let myURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        let myParams = ["count": 10]
+        
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myURL, parameters: myParams, success:{ (tweets: [NSDictionary]) in
+                                                        
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+                
+            }//end for
+            
+            self.tableView.reloadData()
+            
+        }//end Success
+        ,failure: { (Error) in
+            print("could not retrieve tweet")
+            
+        })//end Failure
+        
+    }//end loadTweet
 
-            }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+        return 1
+    }//end numberOfSections
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+        return tweetArray.count
+    }//end tableView
 
     
-}
+}//end Class
